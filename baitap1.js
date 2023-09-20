0e07aba7df3403947174b304fd0367e2f4b0cad3
unknown <hoanglinhnguyen1189@gmail.com>
2023-09-20 19:35:48 +0700

    baitap1

---
+++ baitap1.js
@@ -0,0 +1,15 @@
+//bài 1
+//câu a
+console.log('xin chào các bạn') // in ra màng hình
+//câu b
+console.log('Nguyễn Hoàng Linh')
+// câu c
+console.clear()
+//câu d
+console.log(' *****\n','*****\n','*****\n','*****\n','*****')
+//câu e
+console.log('    *    ')
+console.log('   ***   ')
+console.log('  *****  ')
+console.log(' ******* ')
+console.log('*********')
