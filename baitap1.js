957767d5e557e8499116a14786fc09408072fdf0
unknown <hoanglinhnguyen1189@gmail.com>
2023-09-22 19:29:54 +0700

    baitap1

--- baitap1.js
+++ baitap1.js
@@ -15,8 +15,53 @@ console.log(' ******* ')
 console.log('*********')
 // câu f
 console.log(' **    ** ')
-console.log('*    *    *')
-console.log('')
-console.log('')
-console.log('')
-console.log('')
+console.log('*    *   *')
+console.log('*        *')
+console.log(' *      *')
+console.log('  *    *')
+console.log('     *  ')
+// bài 2
+//câu a
+let a = 10
+console.log(a)
+//câu b
+let b = '10'
+console.log(b)
+// câu c
+ a = 20
+console.log(a)
+// câu d
+const pi = 3.14 // const : khai báo hằng số
+console.log(pi)
+// câu e
+ //pi = 5
+ //console.log(pi)
+ // câu f
+ let sp = {
+    ten : "Iphone 14",
+    HSX : 'Apple',
+    mausac : 'Trắng',
+    Gia : '1000 USD'
+ }
+ sp.mausac = "Xanh"
+ sp.Gia = "750 USD"
+ sp.ten = "Iphone 14 Promax"
+ console.log(sp)
+ console.log('======================')
+// khởi tạo object
+//let maychieu = new Object()
+//let maychieu = {
+  //  masac : "trắng",
+    //trongluong : '15kg',
+    //hangsanxuat : 'abc'
+//}
+let hocsinh = {
+    ten : "Nguyễn Hoàng Linh",
+    tuoi : 35,
+    diachi : '40/1c'
+}
+hocsinh.sdt = 1234546878
+hocsinh.tuoi = 20
+console.log(hocsinh)
+
+
