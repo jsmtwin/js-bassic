fa0debe5c1dd2b56d17c3f15fc7b4aa5304224d9
unknown <hoanglinhnguyen1189@gmail.com>
2023-09-25 19:40:40 +0700

    hamso

---
+++ lab1-function.js
@@ -0,0 +1,12 @@
+function tinhtong(a,b) {
+    const kq = a + b
+    return kq
+}
+const kq1 = tinhtong(20,15)
+console.log(kq1)
+function tich(a,b){
+    const kq = a*b
+    return kq
+}
+const kq2 = tich(3,9)
+console.log(kq2)
