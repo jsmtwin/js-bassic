e3eb1319c90a08fb97d99149f919a26791cd964f
unknown <hoanglinhnguyen1189@gmail.com>
2023-09-25 19:17:30 +0700

    toantu

--- lab1toantu.js
+++ lab1toantu.js
@@ -15,6 +15,34 @@ console.log(tbc)
 let x = 3
 let y = 4
 let z = 6
-let kq1 = x*x + 2*x +1
-console.log(kq1)
-let kq2 = x*x*x -3*x*y
+let kq1 = x*x + 2*x + 1
+console.log("biểu thức 1 :", kq1)
+let kq2 = x*x*x - 3*x*y - 5*y + 3*y*y
+console.log("biểu thức 2 :", kq2)
+let kq3 = (x*y) * (x*y) - 2*x*x*y + 13*y
+console.log("biểu thức 3 : ", kq3)
+let kq4 = 4*x*x*x + 3*x*y + y*y - (2*x*x - 3*y)
+console.log("biểu thức 4 : ", kq4)
+let kq5 = ((5*x*x) / (4*x*y)) + y*y
+console.log("biểu thức 5 : ", kq5)
+let kq6 = x*x - 2*z*x*y / (y*y) + 5*x - 2*y*y + 4*x*z*z*z + z*z*z
+console.log("biểu thức 6 : ", kq6)
+// câu 3 : d - chu vi diện tích hình tròn
+let d = 5
+let r = d/2
+const pi = 3.14
+let cv = 2*r*pi
+let dt = r*r*pi
+console.log("chu vi hình tròn :", cv)
+console.log("diện tích hình tròn :", dt)
+// câu 3 : f - vận tốc
+let s = 120
+let t = 130/60
+let v = s/t
+console.log("vận tốc : ", v)
+
+
+
+
+
+
