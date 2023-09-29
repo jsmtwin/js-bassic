a163461d9d1dcdc91facc6f92138ff8d38329ff7
jsmtwin <hoanglinhnguyen1189@gmail.com>
2023-09-29 18:50:07 +0700

    cau lenh if eles

--- lab1-caulenh if-else.js
+++ lab1-caulenh if-else.js
@@ -1,52 +1,73 @@
-// câu 1 - a kiểm tra âm dương
-function ktraamduong(n){
-    if (n<0){
-        console.log('số n là số âm')
-    }
-    else if (n>0){
-        console.log('số n là số dương')
-    }
-    else{
-        console.log('số n không âm không dương')
-    }
-}
-ktraamduong(0)
-// câu - b kiểm tra chẵn lẻ
-function chan_le (n){
-    if (n%2 ==0) {
-        console.log("%s là số chẵn",n)
-    }
-    else{
-        console.log('%s là số lẻ',n)
-    }
-} 
-chan_le(4)
-//câu 1 - c - tìm số lớn nhất
-function solonnhat (a,b,c) {
-    let max = a
-    if (max<b){
-        max = b
-    }
-    if (max<c){
-        max = c
-    }
-    console.log('số lớn nhất là : %s', max)
-}
-solonnhat(67,11,45)
-// câu 1 - d - kiểm tra năm nhuần
-function kiemtranamnhuan(a){ // kiểm tra năm nhuần
-    if (a % 4 == 0 &&  a % 100 != 0 ){
-        console.log(a, 'năm nay là năm nhuần')
-    }
-    else if (a % 400 == 0) {
-        console.log(a, 'năm nay là năm nhuần')
-    }    
-    else {
-        console.log(a, 'năm nay không phải năm nhuần')
-    }
-}
-kiemtranamnhuan(2000)
-kiemtranamnhuan(2008)
-kiemtranamnhuan(2004)
-kiemtranamnhuan(2010)
+// // câu 1 - a kiểm tra âm dương
+// function ktraamduong(n){
+//     if (n<0){
+//         console.log('số n là số âm')
+//     }
+//     else if (n>0){
+//         console.log('số n là số dương')
+//     }
+//     else{
+//         console.log('số n không âm không dương')
+//     }
+// }
+// ktraamduong(0)
+// // câu - b kiểm tra chẵn lẻ
+// function chan_le (n){
+//     if (n%2 ==0) {
+//         console.log("%s là số chẵn",n)
+//     }
+//     else{
+//         console.log('%s là số lẻ',n)
+//     }
+// } 
+// chan_le(4)
+// //câu 1 - c - tìm số lớn nhất
+// function solonnhat (a,b,c) {
+//     let max = a
+//     if (max<b){
+//         max = b
+//     }
+//     if (max<c){
+//         max = c
+//     }
+//     console.log('số lớn nhất là : %s', max)
+// }
+// solonnhat(67,11,45)
+// // câu 1 - d - kiểm tra năm nhuần
+// function kiemtranamnhuan(a){ // kiểm tra năm nhuần
+//     if (a % 4 == 0 &&  a % 100 != 0 ){
+//         console.log(a, 'năm nay là năm nhuần')
+//     }
+//     else if (a % 400 == 0) {
+//         console.log(a, 'năm nay là năm nhuần')
+//     }    
+//     else {
+//         console.log(a, 'năm nay không phải năm nhuần')
+//     }
+// }
+// kiemtranamnhuan(2000)
+// kiemtranamnhuan(2008)
+// kiemtranamnhuan(2004)
+// kiemtranamnhuan(2010)
 // câu 1 - e kiểm tra kí tự nguyên âm hay phụ âm
+// câu 2  - a
+let a = 2
+let b = 3
+let c = 1
+function phuong_trinh_bac_2 (a,b,c) {
+    let denta = b*b - 4 * a * c
+    let x1 = (-b + Math.sqrt(denta))/(2*a)
+    let x2 = (-b - Math.sqrt(denta))/(2*a)
+    if (denta <0) {
+        return "phương trình vô nghiệm"
+    }
+    else if (denta ==0) {
+        return ' phương trình có nghiệm x1 = x2 = $(-b/(2*a)'
+    }
+    else {
+        const x1 = (-b + Math.sqrt(denta))/(2*a)
+        const x2 = (-b + Math.sqrt(denta))/(2*a) 
+        return (x1, x2)
+    }
+}
+console.log(phuong_trinh_bac_2(a,b,c))
