abced34cb591cc8d92d29881d0384e7f5b4956d5
jsmtwin <hoanglinhnguyen1189@gmail.com>
2023-09-29 19:37:32 +0700

    vong lap

---
+++ vonglap.js
@@ -0,0 +1,7 @@
+// bài 1
+let arr = [3, 451, 5, 2, 6, 76, 3, 5, 1, 243, 6, 4]
+// câu lệnh for 
+let dodaimang = arr.length
+for (let i = 0; i <dodaimang ; i = i +1){
+    console.log('phần tử thứ %s = %s ', i, arr[i])
+}
