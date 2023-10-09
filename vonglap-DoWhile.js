b1fb5a5d36282b8f8ad0d26e2caa48a695475777
jsmtwin <hoanglinhnguyen1189@gmail.com>
2023-10-09 19:26:22 +0700

    vong lap

---
+++ vonglap-DoWhile.js
@@ -0,0 +1,5 @@
+let i = 0
+do{
+    console.log('do whlie :', i)
+    i = i + 1
+}while(i <0)
--- vonglap-While.js
+++ vonglap-While.js
@@ -34,7 +34,7 @@ i = 0
 while(arr[i] !== undefined){
     i = i + 1
 }
-console.log('trung binh cong cua mang la : %s', tong / i)
+console.log('trung binh cong cua mang la : %s', tong / i) // i la do dai mang
 // cau e : dao nguoc cac phan tu
 function caue(){
     let i = 0
@@ -49,4 +49,86 @@ function caue(){
     }
     console.log('cau e', arr)
 }
-caue()
+// caue()
+// cau f : so lan xuat hien phan tu trong mang
+i = 0
+const tansuatxuathien =  {}
+while(arr[i] !== undefined){
+    const phantuthuI = arr[i]
+    tansuatxuathien[phantuthuI] = (tansuatxuathien[phantuthuI] ||0) +1
+    i =i + 1
+}
+console.log('tan suat xuat hien : ', tansuatxuathien)
+// cau g : chen phan tu dau mang
+i = do_dai_mang - 1
+while(arr[i] !== undefined){
+    arr[i + 1] = arr[i]
+    i = i - 1
+}
+arr[0] = 117
+
+// console.log('do dai mang luc sau', arr.length)
+console.log(arr)
+// cau h : kiem tra mang tang dan
+const dodaimangM = arr.length
+let checktangdan = true
+i = 0
+while(arr[i] !==undefined){
+    if(arr[i +1]<arr[i]){
+    i = i + 1
+    checktangdan = false
+    break
+    }
+}
+if(checktangdan == true){
+    console.log('mang tang dan')
+}
+else{
+    console.log('mang khong tang')
+}
+// cau i : sap xep theo thu tu tang dan
+i = 0
+while(arr[i] !== undefined){
+    let j = i + 1
+    while(arr[j] !== undefined){
+        if(arr[i]>arr[j]){
+            let tam = arr[i]
+            arr[i] = arr[j]
+            arr[j] = tam
+        }
+        j = j + 1
+    }
+    i = i + +1
+}
+console.log(arr)
+// cau j : thu tu giam dan
+i = 0
+while(arr[i] !== undefined){
+    let j = i + 1
+    while(arr[j] !== undefined){
+        if(arr[i]<arr[j]){
+            let tam = arr[i]
+            arr[i] = arr[j]
+            arr[j] = tam
+        }
+        j = j + 1
+    }
+    i = i + +1
+}
+console.log(arr)
+//cau k : liet ke so chan trong mang
+i = 0
+while(arr[i] !== undefined){
+    if(arr[i] %2 ===0){
+        console.log('so chan trong mang :',arr[i])    
+    }
+    i = i + 1
+}
+// cau L : liet ke so le
+i = 0
+while(arr[i] !== undefined){
+    if(arr[i] %2 !==0){
+        console.log('so le trong mang :',arr[i])    
+    }
+    i = i + 1
+}
